d5b81b9ecbf7293e027bbb46167cd67e4bf71933
Jits Schilperoort <jits@Jitss-MacBook-Pro.local>
2019-05-02 18:59:57 +0200

    added possibility for different optimizers and activation functions

--- cnn.py
+++ cnn.py
@@ -2,12 +2,14 @@ from __future__ import print_function
 import keras
 from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Dense, Dropout, Flatten, Activation, LeakyReLU
 from keras.layers import Conv2D, MaxPooling2D
 
 batch_size = 128
 num_classes = 10
-epochs = 2
+epochs = 10
+activation = 'relu'  # 'relu' or 'leakyrelu'
+optimizer = 'adadelta'  # 'adadelta' or 'sgd'
 
 # input image dimensions
 img_rows, img_cols = 28, 28
@@ -25,7 +27,6 @@ x_test = x_test.astype('float32')
 x_train /= 255
 x_test /= 255
 
-
 print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
@@ -36,19 +37,37 @@ y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
                  input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
+if activation == 'relu':
+    model.add(Activation('relu'))
+elif activation == 'leakyrelu':
+    model.add(LeakyReLU())
+model.add(Conv2D(64, (3, 3)))
+if activation == 'relu':
+    model.add(Activation('relu'))
+elif activation == 'leakyrelu':
+    model.add(LeakyReLU())
 model.add(MaxPooling2D(pool_size=(2, 2)))
 # model.add(Dropout(0.25))
 model.add(Flatten())
-model.add(Dense(128, activation='relu'))
+model.add(Dense(128))
+if activation == 'relu':
+    model.add(Activation('relu'))
+elif activation == 'leakyrelu':
+    model.add(LeakyReLU())
 # model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
 
-model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adam(),
-              metrics=['accuracy'])
+
+if optimizer == 'sgd':
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.SGD(),
+                  metrics=['accuracy'])
+elif optimizer == 'adadelta':
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adadelta(),
+                  metrics=['accuracy'])
+
 
 model.fit(x_train, y_train,
           batch_size=batch_size,
